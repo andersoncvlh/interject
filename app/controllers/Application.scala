383cdca95338ef5f3d2fb559d7dbab02ecb09299
Andrew Jackson <Andrew.Jackson@bl.uk>
2013-08-29 09:53:07 +0100

    Now actually plays, having added a passthrough action.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,6 +2,11 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.libs.ws.WS
+import play.api.libs.concurrent.Execution.Implicits._
+import play.api.libs.iteratee._
+import scala.concurrent._
+import java.io.InputStream
 
 object Application extends Controller {
   
@@ -9,4 +14,16 @@ object Application extends Controller {
     Ok(views.html.index("Your new application is ready."))
   }
   
+  def passthrough(url: String) = Action {
+    Async {
+      WS.url(url).get().map { response =>
+      val asStream: InputStream = response.ahcResponse.getResponseBodyAsStream
+      Ok.stream(Enumerator.fromStream(asStream)).as("application/octet-stream").withHeaders( CONTENT_TRANSFER_ENCODING -> "binary" )
+      }
+    }
+  }
+  
+  def jsspeccy(url: String) = Action {
+    Ok(views.html.jsspeccy("/action/passthrough/"+url));
+  }
 }
