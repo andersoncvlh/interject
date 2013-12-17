953e69e62b58a97e0880b9f1f53c937c604cdd37
kinmanli <kinman.li@bl.uk>
2013-12-17 13:54:27 +0000

    Removed experimental html/js/css from inspect view. Added a .tap files for test.

--- interject-servlet-filter/src/test/java/uk/bl/wa/interject/type/InterjectionTest.java
+++ interject-servlet-filter/src/test/java/uk/bl/wa/interject/type/InterjectionTest.java
@@ -6,18 +6,24 @@ import java.io.InputStream;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.tika.Tika;
+import org.apache.tika.detect.Detector;
+import org.apache.tika.metadata.Metadata;
 import org.apache.tika.mime.MediaType;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import de.schlichtherle.io.File;
+import uk.bl.wa.nanite.droid.DroidDetector;
 import uk.bl.wa.interject.factory.InterjectionFactory;
+import uk.gov.nationalarchives.droid.command.action.CommandExecutionException;
 
 public class InterjectionTest {
 
-	protected static Logger logger = LogManager.getLogger(InterjectionTest.class);
+	protected static Logger logger = LogManager
+			.getLogger(InterjectionTest.class);
 	private Tika tika = null;
-	
+
 	@Before
 	public void setUp() throws Exception {
 		tika = new Tika();
@@ -69,12 +75,29 @@ public class InterjectionTest {
 		String filename = "/test.tiff";
 		InputStream inputStream = getClass().getResourceAsStream(filename);
 		String sourceType = tika.detect(inputStream);
-		String problemType = InterjectionFactory.INSTANCE.findProblemType(sourceType).getMimeType();
+		String problemType = InterjectionFactory.INSTANCE.findProblemType(
+				sourceType).getMimeType();
 		String expected = MediaType.image("tiff").toString();
 		String mimeType = problemType;
 		Assert.assertEquals(expected, mimeType);
-		System.out.println(String.format(
-				"detected media type for given file %s: %s", filename,
-				mimeType));
+		System.out.println(String
+				.format("detected media type for given file %s: %s", filename,
+						mimeType));
+	}
+
+	@Test
+	public void testSpectrum() {
+		DroidDetector detector;
+		try {
+			detector = new DroidDetector();
+			String f = getClass().getResource("/ZZOOM.TAP").getFile();
+			File file = new File(f);
+			MediaType mediaType = detector.detect(file);
+			Assert.assertNotNull(mediaType);
+			System.out.println("MediaType : " + mediaType);
+		} catch (CommandExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
