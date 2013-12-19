2a445e674356b09cce9f0c40b13d51235c915def
kinmanli <kinman.li@bl.uk>
2013-12-19 11:33:37 +0000

    tidied up and removed unused js files. Tested and working with .tap urls

--- interject-webapp/app/controllers/Application.scala
+++ interject-webapp/app/controllers/Application.scala
@@ -9,19 +9,20 @@ import scala.concurrent._
 import java.io.InputStream
 
 object Application extends Controller {
-  
+
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
-  
+
   def passthrough(url: String) = Action.async {
-      WS.url(url).get().map { response =>
-	      val asStream: InputStream = response.ahcResponse.getResponseBodyAsStream
-	      Ok.chunked(Enumerator.fromStream(asStream)).as("application/octet-stream").withHeaders( CONTENT_TRANSFER_ENCODING -> "binary" )
+    WS.url(url).get().map { response =>
+      val asStream: InputStream = response.ahcResponse.getResponseBodyAsStream
+      Ok.chunked(Enumerator.fromStream(asStream)).as("application/octet-stream").withHeaders(CONTENT_TRANSFER_ENCODING -> "binary")
     }
   }
-  
+
   def jsspeccy(url: String) = Action {
-    Ok(views.html.jsspeccy(url));
+    // call passthrough
+    Ok(views.html.jsspeccy("/action/passthrough/" + url));
   }
 }
--- interject-webapp/app/controllers/Inspect.scala
+++ interject-webapp/app/controllers/Inspect.scala
@@ -18,41 +18,35 @@ import scala.collection.mutable.ArrayBuffer
 import uk.bl.wa.interject.factory.InterjectionFactory
 
 object Inspect extends Controller {
-  
+
   def inspect(url: String) = Action {
-	  // http://www.webarchive.org.uk/interject/action/inspect/http://web.archive.org/web/19991001051504/http://wkweb1.cableinet.co.uk:80/malkc/Wheelie.tap
-	  // 1. identify contents using Apache Tika
-	  val tika = new Tika();
-	  var mimeType = tika.detect(url);
-	  
-	  println("mimeType : " + mimeType)
+    // http://www.webarchive.org.uk/interject/action/inspect/http://web.archive.org/web/19991001051504/http://wkweb1.cableinet.co.uk:80/malkc/Wheelie.tap
+    // 1. identify contents using Apache Tika
+    val tika = new Tika();
+    var mimeType = tika.detect(url);
 
-	  // 2. look up list of actions based on type - DO WE NEED THIS?
-	  val interjection = InterjectionFactory.INSTANCE.findProblemType(mimeType);
-	  if (interjection != null) {
-	    println("interjection : " + interjection)
-	  }
-	  
-	  // 3. Get list of actions based on mime type
-	  
-	  val config = ConfigFactory.load()
-	  // A scala list of SimpleConfigObjects
-	  // just a test for speccy
-	  val options = "." + mimeType
-	  println("Getting actions for mime type : " + options);
-	  var actions = config.getConfigList("mime.type.actions" + options).map(new ActionObject(_))
-	  println(actions.getClass.getName)
+    println("mimeType : " + mimeType)
 
-	  actions.foreach(e => {
-		  println(mimeType + " " + e.getAction() + " " + e.getDescription())
-	  })
+    // 2. look up list of actions based on type - Do we need this?
+    val interjection = InterjectionFactory.INSTANCE.findProblemType(mimeType);
+    if (interjection != null) {
+      println("interjection : " + interjection)
+    }
 
-//	  println("mimeType: " + mimeType + " " + interjection.getRedirectUrl() + " " + interjection.getMimeType())
-//	  Redirect("/action/" + action + "/" + url)
-	  
-	  println("forwarding url : " + url);
-	  Ok(views.html.inspect(url, actions));
+    // 3. Get list of actions based on mime type	  
+    val config = ConfigFactory.load()
+    // A scala list of SimpleConfigObjects
+    // just a test for speccy
+    val options = "." + mimeType
+    println("Getting actions for mime type : " + options);
+    var actions = config.getConfigList("mime.type.actions" + options).map(new ActionObject(_))
+    println(actions.getClass.getName)
 
-	  
-  }  
+    actions.foreach(e => {
+      println(mimeType + " " + e.getAction() + " " + e.getDescription())
+    })
+
+    println("forwarding url : " + url);
+    Ok(views.html.inspect(url, actions));
+  }
 }
