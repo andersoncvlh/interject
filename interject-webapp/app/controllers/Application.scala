ad8021ff8bdc9f30a682118149bf089e457fa004
kinmanli <kinman.li@bl.uk>
2013-12-23 16:40:53 +0000

    Initial check in of feedback task. Currently writes to a file in /tmp/feedback/feedback.txt

--- interject-webapp/app/controllers/Application.scala
+++ interject-webapp/app/controllers/Application.scala
@@ -6,7 +6,10 @@ import play.api.libs.ws.WS
 import play.api.libs.concurrent.Execution.Implicits._
 import play.api.libs.iteratee._
 import scala.concurrent._
-import java.io.InputStream
+import java.io._
+import play.api.libs.Files
+import com.typesafe.config.ConfigFactory
+import com.typesafe.config.Config
 
 object Application extends Controller {
 
@@ -24,5 +27,44 @@ object Application extends Controller {
   def jsspeccy(url: String) = Action {
     // call passthrough
     Ok(views.html.jsspeccy("/action/passthrough/" + url));
+  }
+  
+  def commonsImagingConversion(url: String) = Action {
+    Ok(views.html.jsspeccy("/action/passthrough/" + url));
+  }
+  
+  def imageIOConversion(url: String) = Action {
+    Ok(views.html.jsspeccy("/action/passthrough/" + url));
+  }
+  
+  // -- Javascript routing
+  def javascriptRoutes = Action { implicit request =>
+  	import routes.javascript._
+  	Ok(
+  	  Routes.javascriptRouter("jsRoutes")(
+  	    routes.javascript.Application.rate,
+  	    routes.javascript.Application.sendFeedback
+  	  )
+  	).as("text/javascript")
+  }
+  
+  def rate(entry: String) = Action { implicit request =>
+    writeToFile(entry);
+	Ok("false")
+  }
+
+  def sendFeedback(entry: String) = Action { implicit request =>
+    writeToFile(entry);
+	Ok("false")
+  }
+  
+  def writeToFile(entry: String) = {
+    println(entry);
+    val config = ConfigFactory.load()
+    val path = config.getString("feedback.path");
+    val file = new File(path);
+    var fileContents = Files.readFile(file);
+    fileContents += entry + "\n"; 
+    Files.writeFile(file, fileContents);
   }
 }
